bdfde23ed1a9e57274f2b6e4994862e33a8312f3
guilleballesteros <bugfiher@outlook.com>
2021-02-04 12:09:17 +0100

    base de datos

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 class sugerencias(models.Model):
     _name = "sugerencias"
@@ -6,5 +6,11 @@ class sugerencias(models.Model):
     title = fields.Char("Titulo")
     DeadLine = fields.Date("Fecha Límite")
     details = fields.Char("Detalles")
+    employee = fields.Many2one("hr.employee", "Employee")
 
+class sugerencia_voto(models.Model):
+    _name="sugerencia.voto"
+
+    employee = fields.Many2one("hr.employee", "Employee")
+    vote = fields.Boolean("Agree")
 
